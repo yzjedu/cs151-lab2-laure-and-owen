fa137d9df5f179e4b33dfdbc7e59e7326216154f
oesanchez <oesanchez@loyola.edu>
2024-09-19 12:38:49 -0400

    Lab 02

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 # Enter Code here
 # Read the README - under files on left
 # Delete these three lines before submitting
+between_births = int(input())
+between_deaths = int(input())
+between_immigration = int(input())
+(secs_per_year / births + secs_per_year / immigration - secs_per_year / deaths) * num_years
