3c565c8e2e8a3c9a8901dab55f4e6b17c2958e27
oesanchez <oesanchez@loyola.edu>
2024-09-19 12:56:36 -0400

    Lab 02

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-# Enter Code here
-# Read the README - under files on left
-# Delete these three lines before submitting
-between_births = int(input())
-between_deaths = int(input())
-between_immigration = int(input())
-(secs_per_year / births + secs_per_year / immigration - secs_per_year / deaths) * num_years
+# Professor Z
+# CS151 Lab 02
+# Programmers: Laure and Owen
+
+between_births = print('How many seconds between birth?'),int(input())
+between_deaths = print('How many seconds between deaths'),int(input())
+between_immigration = print('How many seconds between immigration'),int(input())
+population = print('What is the current population'),int(input())
+future_years = print('How many years in the future'),int(input())
+# (secs_per_year / births + secs_per_year / immigration - secs_per_year / deaths) *
+# (secs_per_year / births + secs_per_year / immigration - secs_per_year / deaths) * num_years
