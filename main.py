6098075dd47b553e6c96cd5f33abe7bb4cca0f17
oesanchez <oesanchez@loyola.edu>
2024-09-19 13:03:49 -0400

    Lab 02

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ between_deaths = print('How many seconds between deaths'),int(input())
 between_immigration = print('How many seconds between immigration'),int(input())
 population = print('What is the current population'),int(input())
 future_years = print('How many years in the future'),int(input())
-# (secs_per_year / births + secs_per_year / immigration - secs_per_year / deaths) *
+secs_per_year = 365*24*60
 # (secs_per_year / births + secs_per_year / immigration - secs_per_year / deaths) * num_years
