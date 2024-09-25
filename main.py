d1718bfaf3b5a25a9ea298b44e022f9322824e47
LaurePatera <ampatera@loyola.edu>
2024-09-25 13:07:58 -0400

    Added finished algorithm

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 # CS151 Lab 02
 # Programmers: Laure and Owen
 
-between_births = print('How many seconds between birth?'),int(input())
-between_deaths = print('How many seconds between deaths'),int(input())
-between_immigration = print('How many seconds between immigration'),int(input())
-population = print('What is the current population'),int(input())
-future_years = print('How many years in the future'),int(input())
-secs_per_year = 365*2
+between_births = float(input('How many seconds between birth?'))
+between_deaths = float(input('How many seconds between deaths?'))
+between_immigration = float(input('How many seconds between immigrations?'))
+current_population = float(input('What is the current population?'))
+future_years = float(input('How many years are you looking into the future?'))
+secs_per_year = 365 * 24 * 60 * 60
 # (secs_per_year / births + secs_per_year / immigration - secs_per_year / deaths) * num_years
